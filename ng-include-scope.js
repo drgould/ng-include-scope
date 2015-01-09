a4921327be2ad0008679bda117ec6dd6cf2d50db
Derek Gould <surfrdude11@gmail.com>
2015-01-09 12:57:44 -0800

    Add ngIncludeIsolateScope directive

--- ng-include-scope.js
+++ ng-include-scope.js
@@ -1,24 +1,42 @@
 angular.module('drg.ngIncludeScope', [])
-.directive( 'ngIncludeScope', function() {
+.factory( 'ngIncludeScopeService', function() {
+    'use strict';
+    
+    return function( scope, attrs, field, isolate ) {
+        var keys = [];
+        
+        scope.$watchCollection( attrs[ field ], function( newScope ) {
+            keys = newScope ? Object.keys( newScope ) : [];
+            // set getters
+            angular.forEach( keys, function(key) {
+                if( !scope.hasOwnProperty( key ) ) {
+                    Object.defineProperty( scope, key, { 
+                        get: function() {
+                            return !isolate || ( isolate && !!~keys.indexOf( key ) ) ? scope.$eval( attrs[ field ] + "['" + key.replace("'", "\\'") + "']" ) : undefined;
+                        }
+                    } );
+                }
+            } );
+        } );
+    };
+} )
+.directive( 'ngIncludeScope', [ 'ngIncludeScopeService', function( ngIncludeScopeService ) {
     'use strict';
 
     return {
         restrict: 'A',
         link : function( scope, elem, attrs ) {
-            var keys = [];
-            scope.$watchCollection( attrs.ngIncludeScope, function( newScope ) {
-                keys = newScope ? Object.keys( newScope ) : [];
-                // set getters
-                angular.forEach( keys, function(key) {
-                    if( angular.isUndefined( scope[ key ] ) ) {
-                        Object.defineProperty( scope, key, { 
-                            get: function() {
-                                return !!~keys.indexOf( key ) ? scope.$eval( attrs.ngIncludeScope + "['" + key.replace("'", "\\'") + "']" ) : undefined;
-                            }
-                        } );
-                    }
-                } );
-            } );
+            ngIncludeScopeService( scope, attrs, 'ngIncludeScope', false );
         }
     };
-});
+} ] )
+.directive( 'ngIncludeIsolateScope', [ 'ngIncludeScopeService', function( ngIncludeScopeService ) {
+    'use strict';
+
+    return {
+        restrict: 'A',
+        link : function( scope, elem, attrs ) {
+            ngIncludeScopeService( scope, attrs, 'ngIncludeIsolateScope', true );
+        }
+    };
+} ] );

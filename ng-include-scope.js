1c599acf604644d7b202f37584c9555714bfa847
Derek Gould <surfrdude11@gmail.com>
2015-01-08 16:17:44 -0800

    Simplify and make better :)
    Removed deep watch because it broke on when using scopes. Instead it now sets a series of getters on the scope for each property found on the given scope. Also it's much more performant.

--- ng-include-scope.js
+++ ng-include-scope.js
@@ -5,27 +5,23 @@ angular.module('drg.ngIncludeScope', [])
     return {
         restrict: 'A',
         link : function( scope, elem, attrs ) {
+            var keys = [];
             scope.$watch( attrs.ngIncludeScope, function( newScope, oldScope ) {
-                var key,
-                    newKeys = Object.keys( newScope ),
-                    oldKeys = Object.keys( oldScope );
+                var key;
                 
-                // update values
-                for( key in newKeys ) {
-                    key = newKeys[ key ];
-                    if( newScope.hasOwnProperty( key ) ) {
-                        scope[ key ] = newScope[ key ];
-                    }
-                }
+                keys = Object.keys( newScope );
                 
-                // remove old values
-                for( key in oldKeys ) {
-                    key = oldKeys[ key ];
-                    if( typeof newScope[ key ] === 'undefined' ) {
-                        delete scope[ key ];
+                // set getters
+                angular.forEach( keys, function(key) {
+                    if( newScope.hasOwnProperty( key ) && angular.isUndefined( scope[ key ] ) ) {
+                        Object.defineProperty( scope, key, { 
+                            get: function() {
+                                return !!~keys.indexOf( key ) ? scope.$eval( attrs.ngIncludeScope + "['" + key.replace("'", "\\'") + "']" ) : undefined;
+                            }
+                        } );
                     }
-                }
-            }, true );
+                } );
+            } );
         }
     };
 });
